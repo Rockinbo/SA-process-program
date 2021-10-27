cbef8ccb5c7890f72af5879c2528f265a404897b
Rockinbo <39218335+Rockinbo@users.noreply.github.com>
2021-10-27 09:16:46 +0800

    Update RPY2T.m
    modify

--- RPY2T.m
+++ RPY2T.m
@@ -3,10 +3,10 @@ function T=RPY2T(x,y,z,a,b,c)
 %Output: HTM
 %Made by: Rockinbo
 %2021-10-20
-%RotZ=a，RotY=b, RotX=c  unit:deg,mm
+%RotZ=c，RotY=b, RotX=a  unit:deg,mm
 
 
-R=[cosd(a) -sind(a) 0;sind(a) cosd(a) 0;0 0 1;]*[cosd(b) 0 sind(b);0 1 0;-sind(b) 0 cosd(b);]*[1 0 0;0 cosd(c) -sind(c);0 sind(c) cosd(c);];
+R=[cosd(c) -sind(c) 0;sind(c) cosd(c) 0;0 0 1;]*[cosd(b) 0 sind(b);0 1 0;-sind(b) 0 cosd(b);]*[1 0 0;0 cosd(a) -sind(a);0 sind(a) cosd(a);];
 
 P=[x;y;z];
 T=[R,P;0 0 0 1];
